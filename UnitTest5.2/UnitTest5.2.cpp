99c8ddea52ee5fcdd6896b8fbd5d89314f0d5e63
MaxOsadshyi <maksym.osadchyi.ik.2023@lpnu.ua>
2023-10-18 20:58:07 +0300

    maxtest5.1

---
+++ UnitTest5.2/UnitTest5.2.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project5.2/project5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+			
+		}
+	};
+}
---
+++ UnitTest5.22/UnitTest5.22.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project5.22/project5/22.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest522
+{
+	TEST_CLASS(UnitTest522)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+
+		}
+	};
+}
